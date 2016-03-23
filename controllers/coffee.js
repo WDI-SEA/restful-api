a17bfc7e993b1ede498d0e4925898333e1304f0d
michaelscotthurley <michaelscotthurley>
2016-03-22 22:28:32 -0700

    Coffee tested and working

--- controllers/coffee.js
+++ controllers/coffee.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var User = require('../models/coffee');
+var Coffee = require('../models/coffee');
 var router = express.Router();
 
 router.get('/', function(req, res) {
--- index.js
+++ index.js
@@ -10,11 +10,10 @@ var secret = "yesterday_upon_the_stair_I_met_a_man_who_wasnt_there";
 
 mongoose.connect('mongodb://localhost:27017/coffeeauthusers');
 
-app.use("/api/coffees", expressJWT({ secret: secret }).unless({ method: "POST" }));
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/api/users', require('./controllers/users'));
 app.use('/api/coffees', require('./controllers/coffee'));
+app.use("/api/coffees", expressJWT({ secret: secret }).unless({ method: "POST" }));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
